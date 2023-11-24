7aafc35d88fe2461b5d71d9851e4486658fc0323
Ranjani KR <ranjanikuduvalliraveendra@gmail.com>
2023-11-24 15:58:32 +1100

    Updated successful resgister flow to have unique email and validate response body

--- cypress/e2e/userSignup.spec.js
+++ cypress/e2e/userSignup.spec.js
@@ -1,19 +1,27 @@
 /// <reference types="cypress" /> 
 describe('User SignUp Flow', () => {
-    it('Successfully register a user', () => {
+    it('Successfully register a user and validates email,token_type and refresh_token', () => {
+        //Generating a unique email for register flow
+        const uniqueEmail = `abc${Date.now()}@gmail.com`;
       cy.request({
         method:"POST",
+      
         url:"/api/auth/signup",
         body:{
-            email : "chandler.fri@gmail.com",
+            email : uniqueEmail,
             password : "couldIBeMoreFunny",
-
         },
     }).then((response) => {
         cy.log(response.body);
         expect(response.status).to.eq(201);
         expect(response.body).to.have.property("data");
         expect(response.body.data).to.have.keys("user", "session");
+
+        //Deep property assertions
+        expect(response.body.data.user.email).to.eq(uniqueEmail);
+        expect(response.body.data.session.token_type).to.eq("bearer");
+        expect(response.body.data.session.refresh_token).to.not.be.empty;
+
       });
     });
 });
